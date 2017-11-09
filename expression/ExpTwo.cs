465bced570f10b6b62788604f80f7c410038f095
Jack Jiang <jsc723@berkeley.edu>
2017-11-09 10:26:12 -0800

    first commit

---
+++ expression/ExpTwo.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace expression
+{
+    public abstract class ExpTwo : IExpression
+    {
+        public virtual string asString()
+        {
+            bool omit = false,needpr = false;
+            if (u is Number && (v is Variable || v is ExpOne)) omit = true;
+            string left = u.asString(), right = v.asString(),mid = this.name;
+            if (needpr || u.primarity < this.primarity) left = "(" + left + ")";
+            if (needpr || v.primarity <= this.primarity) right = "(" + right + ")";
+            if (mid == "*" && omit && !(Tools.charIsNumber(left[left.Length - 1]) && Tools.charIsNumber(right[0])))
+                mid = "";
+            return left + mid + right;
+        }
+        public abstract double eval(Frame frame);
+        public abstract IExpression deriv(Variable x,ref Frame frame);
+        public abstract IExpression simplify();
+        public IExpression u,v;
+        public string name { get; set; }
+        public abstract int primarity { get; }
+    }
+    public class Add : ExpTwo
+    {
+        public Add(IExpression e1, IExpression e2)
+        {
+            u = e1; v = e2; name = "+"; 
+        }
+        public override double eval(Frame frame = null)
+        { return u.eval(frame) + v.eval(frame); }
+        public override int primarity { get { return 1; } }
+        public override IExpression deriv(Variable x ,ref Frame frame)
+        { return Tools.makeAdd(u.deriv(x,ref frame), v.deriv(x,ref frame)); }
+        public override IExpression simplify()
+        {
+            return Tools.makeAdd(u, v);
+        }
+    }
+    public class Sub : ExpTwo
+    {
+        public Sub(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "-"; }
+        public override double eval(Frame frame = null)
+        { return u.eval(frame) - v.eval(frame); }
+        public override int primarity { get { return 1; } }
+        public override IExpression deriv(Variable x,ref Frame frame)
+        { return Tools.makeSub(u.deriv(x,ref frame), v.deriv(x,ref frame)); }
+        public override IExpression simplify()
+        {
+            return Tools.makeSub(u, v);
+        }
+    }
+    public class Mul : ExpTwo
+    {
+        public Mul(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "*"; }
+        public override double eval(Frame frame = null)
+        { return u.eval(frame) * v.eval(frame); }
+        public override int primarity { get { return 2; } }
+        public override IExpression deriv(Variable x,ref Frame frame) 
+        { return Tools.makeAdd(Tools.makeMul(u.deriv(x,ref frame),v), Tools.makeMul(u, v.deriv(x,ref frame))); }
+        public override IExpression simplify()
+        {
+            return Tools.makeMul(u, v);
+        }
+    }
+    public class Div : ExpTwo
+    {
+        public Div(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "/"; }
+        public override double eval(Frame frame = null)
+        { return u.eval(frame) / v.eval(frame); }
+        public override int primarity { get { return 2; } }
+        public override IExpression deriv(Variable x, ref Frame frame)
+        {
+            IExpression t1 = u.deriv(x, ref frame);
+            IExpression n = Tools.makeSub(Tools.makeMul(u.deriv(x, ref frame), v), Tools.makeMul(u, v.deriv(x, ref frame)));
+            IExpression d = Tools.makePow(v, new Number(2));
+            return Tools.makeDiv(n, d);
+        }
+        public override IExpression simplify()
+        {
+            return Tools.makeDiv(u, v);
+        }
+    }
+    public class Mod : ExpTwo
+    {
+        public Mod(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "%"; }
+        public override double eval(Frame frame = null)
+        { return (int)u.eval(frame) % (int)v.eval(frame); }
+        public override int primarity { get { return 2; } }
+        public override IExpression deriv(Variable x, ref Frame frame)
+        {
+            throw new Exception("求余运算不能求导");
+        }
+        public override IExpression simplify()
+        {
+            return Tools.makeMod(u, v);
+        }
+    }
+    public class Pow : ExpTwo
+    {
+        public Pow(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "^"; }
+        public override double eval(Frame frame = null)
+        { return  Math.Pow(u.eval(frame) , v.eval(frame)); }
+        public override int primarity { get { return 3; } }
+        public override IExpression deriv(Variable x,ref Frame frame)
+        {
+            if (v is Number)
+            {
+                Number k = new Number(v.eval() - 1);
+                IExpression d = Tools.makeMul(v,Tools.makePow(u,k));
+                return Tools.makeMul(d, u.deriv(x,ref frame));
+            }
+            else if (u is Number)
+            {
+                return Tools.makeMul(Tools.makeMul(new Number(Math.Log(u.eval())), this), v.deriv(x,ref frame));
+            }
+            else
+            {
+                IExpression t1 = Tools.makeMul(v.deriv(x,ref frame), new Ln(u));
+                IExpression t2 = Tools.makeMul(Tools.makeDiv(v, u), u.deriv(x,ref frame));
+                return Tools.makeMul(this, Tools.makeAdd(t1, t2));
+            }
+        }
+        public override IExpression simplify()
+        {
+            return Tools.makePow(u, v);
+        }
+    }
+    public class Log : ExpTwo
+    {
+        public Log(IExpression e1, IExpression e2)
+        { u = e1; v = e2; name = "log"; }
+        public override double eval(Frame frame = null)
+        { return Math.Log(v.eval(frame),u.eval(frame)); }
+        public override int primarity { get { return 4; } }
+        public override IExpression deriv(Variable x,ref Frame frame)
+        {
+            if (u is Number)
+            {
+                IExpression t = Tools.makeMul(v, new Ln(u));
+                IExpression t1 = Tools.makeDiv(v.deriv(x, ref frame), t);
+                return t1;
+            }
+            else
+            {
+                IExpression t = Tools.makeDiv(new Ln(v), new Ln(u));
+                return t.deriv(x,ref frame);
+            }
+        }
+        public override string asString()
+        {
+            bool omit = false;
+            if (u is Number && (v is Variable || v is ExpOne)) omit = true;
+
+            string left = "log("+u.asString(), right = v.asString()+")", mid = ", ";
+            if (mid == "*" && omit && !(Tools.charIsNumber(left[left.Length - 1]) && Tools.charIsNumber(right[0])))
+                mid = "";
+            return left + mid + right;
+        }
+        public override IExpression simplify()
+        {
+            return Tools.makeLog(u, v);
+        }
+    }
+
+    
+    
+}
---
+++ expression/Expression.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace expression
+{
+    
+    
+    public interface IExpression 
+    {
+        double eval(Frame frame = null);
+        IExpression deriv(Variable x,ref Frame frame);
+        IExpression simplify();
+        string asString();
+        string name { get; }
+        int primarity { get; }
+    }
+    public abstract class ExpZero : IExpression
+    {
+        public abstract double eval(Frame frame);
+        public abstract IExpression deriv(Variable v,ref Frame frame);
+        public IExpression simplify() { return this; }
+        public abstract string asString();
+        public abstract string name { get; set; }
+        public int primarity { get { return 5; } }
+    }
+    public class Null : ExpZero
+    {
+        public override double eval(Frame frame = null) { throw new Exception("Null对象不能求值"); }
+        public override IExpression deriv(Variable v,ref Frame frame)
+        { throw new Exception("Null对象不能求导"); }
+        public override string asString() { return "null"; }
+        public override string name { get { return asString(); } set { } }
+    }
+    public class Arr : ExpZero
+    {
+        public Arr()
+        {
+            array = new List<IExpression>();
+        }
+        public Arr(List<int> dims,int current = 0)
+        {
+            array = new List<IExpression>();
+            if (current == dims.Count - 1)
+            {
+                for (int i = 0; i < dims[current]; i++)
+                    array.Add(new Null());
+            }
+            else
+            {
+                for (int i = 0; i < dims[current]; i++)
+                    array.Add(new Arr(dims, current + 1));
+            }
+        }
+        public void Add(IExpression e) { array.Add(e); }
+        public IExpression getItem(int i) { return array[i]; }
+        public void setItem(int i, IExpression value) { array[i] = value; }
+        public void setItem(List<int> indexs, int current, IExpression value)
+        {
+            if(current==indexs.Count-1)
+                setItem(indexs[current], value);
+            else
+                ((Arr)array[indexs[current]]).setItem(indexs, current + 1, value);
+        }
+        public override double eval(Frame frame = null) { throw new Exception("Array对象不能求值"); }
+        public override IExpression deriv(Variable v,ref Frame frame) 
+        {
+            Arr temp = new Arr();
+            foreach (var e in array)
+                temp.Add(e.deriv(v,ref frame));
+            return temp;
+        }
+        public override string asString() 
+        {
+            List<Arr> temp = new List<Arr>();
+            return asStringHelper(ref temp);
+        }
+        private string asStringHelper(ref List<Arr> note)
+        {
+            string buf = "{ ";
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (i > 0) buf += ", ";
+                if (array[i] is Arr)
+                {
+                    if (note.Contains(array[i]))
+                        buf += "{...}";
+                    else
+                    {
+                        note.Add((Arr)array[i]);
+                        buf += ((Arr)array[i]).asStringHelper(ref note);
+                    }
+                }
+                else
+                {
+                    buf += array[i].asString();
+                }
+            }
+            buf += " }";
+            return buf;
+        }
+        public override string name { get; set; }
+        public List<IExpression> array;
+    }
+    public class Str : ExpZero
+    {
+        public Str(string value = "")
+        {
+            this.value = value;
+        }
+        public override double eval(Frame frame = null) { throw new Exception("String对象不能求值"); }
+        public override IExpression deriv(Variable v,ref Frame frame)
+        { throw new Exception("String对象不能求导"); }
+        public override string asString() { return value; }
+        public override string name { get; set; }
+        public string value;
+    }
+    public class Number : ExpZero
+    {
+        public Number(double num)
+        {
+            value = num;
+        }
+        public double eval() { return value; }
+        public override double eval(Frame frame = null){ return value; }
+        public override IExpression deriv( Variable v ,ref Frame frame) { return new Number(0); }
+        public override string asString() { return Math.Round(value,Tools.precise).ToString(); }
+        public override string name { get { return asString(); } set { } }
+        private double value;
+    }
+    public class Variable : ExpZero
+    {
+        public Variable(string name)
+        {
+            this.name = name;
+        }
+        public override double eval(Frame frame)
+        {
+            if (frame.containsVarValue(name))
+                return frame.getVarValue(name);
+            if (frame.containsFunc(name))
+                return frame.getFunc(name).eval(frame);
+            throw new Exception("变量\""+name+"\"还没有被赋值");
+        }
+        public override IExpression deriv(Variable v,ref Frame frame)
+        {
+            if (name == v.name)
+                return new Number(1);
+            if (frame.containsFunc(name))
+            {
+                return frame.getFunc(name).deriv(v, ref frame);
+            }
+            return new Number(0); 
+        }
+        public override string asString() { return name; }
+        public override string name { get; set; }
+    }
+    
+    public class Tools
+    {
+        static public int precise = 3;
+        static public string[] fours = { "sin", "cos", "tan", "cot", "exp", "log", "ln","int","eval"};
+        static public string[] threes = { "^" };
+        static public string[] twos = { "*","/","%" };
+        static public string[] ones = { "+", "-" };
+        static public string[] zeros = { "==", ">>", "<<", "!=","<=",">=" };//这里改了记得去toTokens里面加上
+        static public string[] negones = { "&&", "||" };
+        static public string[] negtwos = { "=" };
+        static public string[] paras = { "(", ")","{","}"};
+        static public Frame globalFrame = new Frame();
+        public static void print(IExpression e) { Console.WriteLine(e.asString()); }
+        
+        public static bool charIsNumber(char c)
+        {
+            return c > '0' && c < '9';
+        }
+        public static Number getCoef(IExpression e)
+        {
+            e = e.simplify();
+            if (e is Number) return (Number)e;
+            if (e is Mul)
+            {
+                Mul m = (Mul)e;
+                if (m.u is Number) return (Number)m.u;
+            }
+            if (e is Div)
+            {
+                Div d = (Div)e;
+                if (d.v is Number) return new Number(1 / d.v.eval());
+            }
+            return new Number(1);
+        }
+        public static IExpression getNonCoef(IExpression e)
+        {
+            if (e is Number) return new Number(1);
+            if (e is Mul)
+            {
+                Mul m = (Mul)e;
+                if (m.u is Number) return m.v;
+            }
+            if (e is Div)
+            {
+                Div d = (Div)e;
+                if (d.v is Number) return d.u;
+            }
+            return e;
+        }
+        //equalStruct: cos(u(3x)) == 2cos(u(3x), cos(u(3x)) == cos(u(3x))/3 , 2 == 3,cos(u(3x)) != cos(u(x))
+        //是不是同类项
+        public static bool equalStruct(IExpression e1, IExpression e2)
+        {
+            e1 = e1.simplify(); e2 = e2.simplify();
+            if (e1 is ExpZero && e2 is ExpZero)
+            {
+                if (e1 is Number && e2 is Number) return true;
+                if (e1 is Variable && e2 is Variable && e1.asString() == e2.asString()) return true;
+            }
+            if (e1 is ExpOne && e2 is ExpOne)
+            {
+                if (e1.name == e2.name) return (equal(((ExpOne)e1).u, ((ExpOne)e2).u));
+            }
+            if (equal(getNonCoef(e1), getNonCoef(e2))) return true;
+            if (e1 is ExpTwo && e2 is ExpTwo)
+            {
+                if( ((e1 is Mul && e2 is Mul) || (e1 is Add && e2 is Add))
+                     &&(equalStruct(((ExpTwo)e1).u, ((ExpTwo)e2).v) && equalStruct(((ExpTwo)e1).v, ((ExpTwo)e2).u)))
+                    return true;
+                return (equalStruct(((ExpTwo)e1).u, ((ExpTwo)e2).u) && equalStruct(((ExpTwo)e1).v, ((ExpTwo)e2).v));
+            }
+            return false;
+        }
+        //是不是完全相等
+        public static bool equal(IExpression e1, IExpression e2)
+        {
+            e1 = e1.simplify(); e2 = e2.simplify();
+            if (e1 is ExpZero && e2 is ExpZero)
+            {
+                if (e1 is Number && e2 is Number) return ((Number)e1).eval() == ((Number)e2).eval();
+                if (e1 is Variable && e2 is Variable && e1.name == e2.name) return true;
+            }
+            if (e1 is ExpOne && e2 is ExpOne)
+            {
+                if (e1.name == e2.name) return (equal(((ExpOne)e1).u, ((ExpOne)e2).u));
+            }
+            if (e1 is ExpTwo && e2 is ExpTwo)
+            {
+                if (((e1 is Mul && e2 is Mul) || (e1 is Add && e2 is Add))
+                     && (equal(((ExpTwo)e1).u, ((ExpTwo)e2).v) && equal(((ExpTwo)e1).v, ((ExpTwo)e2).u)))
+                    return true;
+                if(e1.name==e2.name)
+                    return (equal(((ExpTwo)e1).u, ((ExpTwo)e2).u) && equal(((ExpTwo)e1).v, ((ExpTwo)e2).v));
+            }
+            return false;
+        }
+        
+        public static IExpression makeAdd(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            if (u is Number && ((Number)u).eval() == 0) return v;
+            if (v is Number && ((Number)v).eval() == 0) return u;
+            if (v is Number && u is Number) return new Number(v.eval() + u.eval());
+            if (equalStruct(u, v))return makeMul(new Number(getCoef(u).eval() + getCoef(v).eval()), getNonCoef(u));
+            double vcoef = getCoef(v).eval();
+            if (vcoef < 0) return makeSub(u, makeMul(new Number(-1 * vcoef), getNonCoef(v)));
+            return new Add(u, v);
+        }
+        public static IExpression makeSub(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            if (u is Number && ((Number)u).eval() == 0) return makeMul(new Number(-1), v);
+            if (v is Number && ((Number)v).eval() == 0) return u;
+            if (u is Number && v is Number) return new Number(u.eval() - v.eval());
+            if (equalStruct(u, v)) return makeMul(new Number(getCoef(u).eval() - getCoef(v).eval()), getNonCoef(u));
+            double vcoef = getCoef(v).eval();
+            if (vcoef < 0) return makeAdd(u, makeMul(new Number(-1 * vcoef), getNonCoef(v)));
+            return new Sub(u, v);
+        }
+        public static IExpression makeMul(IExpression u, IExpression v)
+        {
+            if (v is Number && !(u is Number)) { IExpression t = u; u = v; v = t; }
+            u = u.simplify(); v = v.simplify();
+            //print(u); print(v);
+            if (u is Number && ((Number)u).eval() == 0) return new Number(0);
+            if (v is Number && ((Number)v).eval() == 0) return new Number(0);
+            if (u is Number && ((Number)u).eval() == 1) return v;
+            if (v is Number && ((Number)v).eval() == 1) return u;
+            if (v is Number && u is Number)  return new Number(v.eval() * u.eval());
+            double vcoef = getCoef(v).eval();
+            if (u is Number && vcoef != 1) 
+                return makeMul(new Number(u.eval() * vcoef), getNonCoef(v));
+            return new Mul(u, v);
+        }
+        public static IExpression makeDiv(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            if (u is Number && u.eval() == 0) return new Number(0);
+            if (u is Number && v is Number) return new Number(u.eval() / v.eval());
+            if (equal(getNonCoef(u), getNonCoef(v)))
+                return new Number(getCoef(u).eval() + getCoef(v).eval());
+            return new Div(u, v);
+        }
+        public static IExpression makeMod(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            return new Mod(u, v);
+        }
+        public static IExpression makePow(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            if (u is Number && v is Number) return new Number(Math.Pow(u.eval() , v.eval()));
+            if (u is Number && u.eval() == 0) return new Number(0);
+            if (v is Number && v.eval() == 0) return new Number(1);
+            if (v is Number && v.eval() == 1) return u;
+            return new Pow(u, v);
+        }
+        public static IExpression makeLog(IExpression u, IExpression v)
+        {
+            u = u.simplify(); v = v.simplify();
+            if (u is Number && v is Number) return new Number(Math.Log(v.eval() , u.eval()));
+            if (v is Number && v.eval() == 1) return new Number(0);
+            if (equal(u, v)) return new Number(1);
+            return new Log(u, v);
+        }
+        public static IExpression makeExp(IExpression u)
+        {
+            u = u.simplify();
+            if (u is Number && u.eval() == 0) return new Number(1);
+            return new Exp(u);
+        }
+        public static bool determine(string[] cond, Frame frame)
+        {
+            if (Program.isWholeArg(cond))
+                return determine(Program.subTokens(cond, 1, cond.Length - 2), frame);
+            if (cond.Length == 1)
+            {
+                if (cond[0] == "0" || cond[0] == "false")
+                    return false;
+                if (cond[0] == "true")
+                    return true;
+                return (Program.makeExpr(cond, ref frame).eval(frame) != 0) ? true : false;
+            }
+            int i = Program.goForwardArg(cond, 0);
+            string mid = cond[i];
+            string[] sleft = Program.subTokens(cond, 0, i),sright = Program.subTokens(cond, i + 1);
+            if (mid == "&&")
+                return determine(sleft, frame) && determine(sright,frame);
+            else if(mid == "||")
+                return determine(sleft, frame) || determine(sright,frame);
+                
+            IExpression left, right;
+            left = Program.makeExpr(sleft, ref frame);
+            right = Program.makeExpr(sright, ref frame);
+            if (left is Str && right is Str)
+            {
+                Str sl = (Str)left;
+                Str sr = (Str)right;
+                if (mid == "==")
+                    return sl.asString().CompareTo(sr.asString()) == 0;
+                else if (mid == "!=")
+                    return sl.asString().CompareTo(sr.asString()) != 0;
+                else if (mid == "<<")
+                    return sl.asString().CompareTo(sr.asString())<0;
+                else if (mid == ">>")
+                    return sl.asString().CompareTo(sr.asString()) > 0;
+                else if (mid == "<=")
+                    return sl.asString().CompareTo(sr.asString()) <= 0;
+                else if (mid == ">=")
+                    return sl.asString().CompareTo(sr.asString()) >= 0;
+            }
+            else if (mid == "==")
+            {
+                if (equal(left, right)) return true;
+                if (left.eval(frame) == right.eval(frame)) return true;
+                return false;
+            }
+            else if (mid == "!=")
+            {
+                if (equal(left, right)) return false;
+                if (left.eval(frame) == right.eval(frame)) return false;
+                return true;
+            }
+            else if (mid == "<<")
+                return (left.eval(frame) < right.eval(frame));
+            else if (mid == ">>")
+                return (left.eval(frame) > right.eval(frame));
+            else if (mid == "<=")
+                return (left.eval(frame) <= right.eval(frame));
+            else if (mid == ">=")
+                return (left.eval(frame) >= right.eval(frame));
+            return false;
+        }
+    }
+}
